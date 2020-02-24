545055da96f42abeb9906d369e2ad1841bdfd473
shubham2705 <shubhamgupta1381@gmail.com>
2020-02-24 23:19:18 +0530

    first commit

---
+++ src/main/java/com/techpro/shb/ShbApp1Application.java
@@ -0,0 +1,13 @@
+package com.techpro.shb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShbApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShbApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techpro/shb/ShbApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techpro.shb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShbApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
